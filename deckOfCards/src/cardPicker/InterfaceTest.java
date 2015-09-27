5af67331df43c541b9de6adb0d4b8b81bd413684
Kristaphonie <VegetaBuddie01@hotmail.com>
2015-09-27 13:45:20 -0700

    stuffs

---
+++ deckOfCards/src/cardPicker/InterfaceTest.java
@@ -0,0 +1,13 @@
+package cardPicker;
+
+import java.util.List;
+
+/**
+ * Created by Chris on 9/20/2015.
+ */
+public interface InterfaceTest {
+
+    void methodOne(int a, int b);
+
+    void methodTwo(String c, List<Integer> d);
+}
---
+++ deckOfCards/src/cardPicker/LambdaPractice.java
@@ -0,0 +1,14 @@
+package cardPicker;
+
+import java.util.List;
+
+/**
+ * Created by Chris on 9/20/2015.
+ */
+public class LambdaPractice {
+
+//    Pickacard a = () ->
+//    {
+//        System.out.println("Hello");
+//    }
+}
